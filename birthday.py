899fc947ed5686edba1eb44133e85efe05350181
Gaurav Bhati <44238868+Bhati123@users.noreply.github.com>
2019-10-22 23:29:13 +0530

    Update birthday.py
    correct

--- birthday.py
+++ birthday.py
@@ -7,7 +7,7 @@ Assignment:
 Your program will ask the user the following questions, in this order:
 
 1. Their name.
-2. The name of the month they were born in (e.g. "September").
+2. The name of the month they were born in (e.g. "September")./////vghvvb
 3. The year they were born in (e.g. "1962").
 4. The day they were born on (e.g. "11").
 
